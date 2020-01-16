59d659edd633e8c0320bcffa23658c0d43118472
Katerina <katerinalupacheva@gmail.com>
2020-01-16 18:15:19 +0300

    Manifest.json and index.js updated due to conversion to PWA

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -15,3 +16,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+serviceWorker.register();
