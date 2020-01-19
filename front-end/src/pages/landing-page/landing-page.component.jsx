f71085ae75f3bbdf0f4e43eda359f7b33ada8681
Katerina <katerinalupacheva@gmail.com>
2020-01-19 22:06:48 +0300

    Main section added to landing page

--- front-end/src/pages/landing-page/landing-page.component.jsx
+++ front-end/src/pages/landing-page/landing-page.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './landing-page.styles.scss';
 
 const LandingPage = () => (
     <div className='landing-page-container'>
-        Landing Page
+        <div className='main-section'>
+            <h1 className='main-title'>Tabata App</h1>
+            <h4 className='main-subtitle'>Simple Way To Be Fit</h4>
+            <Link to='/'>
+                <button className='cta-button'>Get Started</button>
+            </Link>
+        </div>
     </div>
 );
 
 export default LandingPage;
 
 //add subscribe to updates https://vanido.io/
+//stages how to start workout
+//benefits
