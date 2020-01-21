17d947f6d0750d177b137418540053cb9f75a40b
Katerina <katerinalupacheva@gmail.com>
2020-01-21 12:51:11 +0300

    Card component added

---
+++ front-end/src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaRandom } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
+import './card.styles.scss';
+
+const Card = ({ title, description }) => (
+  <div className='card-container'>
+    <div className='card-icon'>
+      <IconContext.Provider value={{ color: '#E0314B', size: "7em" }}>
+        <FaRandom />
+      </IconContext.Provider>
+    </div>
+    <div className='card-title'>
+      {title}
+    </div>
+    <div className='card-description'>
+      {description}
+    </div>
+  </div>
+);
+
+export default Card;
