c248c41f93fff57ec517688f3da6184c2fdcaf80
Katerina <katerinalupacheva@gmail.com>
2020-01-24 12:34:01 +0300

    Noopener noreferrer attribute added to footer icon component

--- front-end/src/components/footer-icon/footer-icon.component.jsx
+++ front-end/src/components/footer-icon/footer-icon.component.jsx
@@ -5,7 +5,7 @@ import './footer-icon.styles.scss';
 
 const FooterIcon = ({ id, link, icon }) => (
     <div className='footer-icon'>
-        <a href={link} target='_blank' key={id}>
+        <a href={link} target='_blank' rel="noopener noreferrer" key={id}>
             <IconContext.Provider value={{ size: "2em" }}>
                 {icon}
             </IconContext.Provider>
