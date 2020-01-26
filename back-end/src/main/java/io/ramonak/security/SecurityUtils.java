48ef1494175f59978b2041c66ca0e14508aa1158
Katerina <katerinalupacheva@gmail.com>
2020-01-26 20:43:46 +0300

    Security utils added

---
+++ back-end/src/main/java/io/ramonak/security/SecurityUtils.java
@@ -0,0 +1,29 @@
+package io.ramonak.security;
+
+import com.vaadin.flow.server.ServletHelper;
+import com.vaadin.flow.shared.ApplicationConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Stream;
+
+public final class SecurityUtils {
+
+    private SecurityUtils() {
+        // Util methods only
+    }
+
+    /**
+     * Tests if the request is an internal framework request. The test consists of
+     * checking if the request parameter is present and if its value is consistent
+     * with any of the request types know.
+     *
+     * @param request
+     *            {@link HttpServletRequest}
+     * @return true if is an internal framework request. False otherwise.
+     */
+    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
+        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+        return parameterValue != null
+                && Stream.of(ServletHelper.RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
+    }
+}
