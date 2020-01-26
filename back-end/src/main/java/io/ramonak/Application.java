28f94b17c3868d4f4111838edb2de88c9e182eea
Katerina <katerinalupacheva@gmail.com>
2020-01-26 20:45:46 +0300

    Password Encoder bean added

--- back-end/src/main/java/io/ramonak/Application.java
+++ back-end/src/main/java/io/ramonak/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * The entry point of the Spring Boot application.
@@ -21,6 +23,11 @@ public class Application extends SpringBootServletInitializer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
