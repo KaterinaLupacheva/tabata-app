21fb6670c41009d69d556e427b70bfac72673258
Katerina <katerinalupacheva@gmail.com>
2020-01-27 14:47:52 +0300

    CORS added

--- back-end/src/main/java/io/ramonak/Application.java
+++ back-end/src/main/java/io/ramonak/Application.java
@@ -9,6 +9,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * The entry point of the Spring Boot application.
@@ -28,6 +30,16 @@ public class Application extends SpringBootServletInitializer {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://tabata.ramonak.io");
+            }
+        };
     }
 
 }
