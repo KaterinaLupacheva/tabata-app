95e65c303b8e345e6cb56978bbeeffd1e05c15a2
Katerina <katerinalupacheva@gmail.com>
2020-01-29 10:30:57 +0300

    Dumbbell Icon component added and is shown on create workout page

--- front-end/src/components/exercise-button/exercise-button.component.jsx
+++ front-end/src/components/exercise-button/exercise-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import DumbbellIcon from '../icons/dumbbell-icon.component';
 
 import './exercise-button.styles.scss';
 import { toggleButtonPressed } from '../../redux/exercises/exercises.actions';
@@ -13,6 +14,9 @@ const ExerciseButton = ({ exercise, toggleExercise, toggleButtonPressed }) => {
             onClick={() => {toggleButtonPressed(exercise); toggleExercise(exercise)}} >
                 <div className='exercise-name'>     
                     {name.toLowerCase()}
+                    {exercise.isWithWeights ? 
+                        <DumbbellIcon /> : 
+                        ''}
                 </div>
         </div>
     )
