025fbac4269f7e39eb4e4b57af6a925c13a1a7bc
Katerina <katerinalupacheva@gmail.com>
2020-01-29 10:47:22 +0300

    Checkbox component added

---
+++ front-end/src/components/checkbox/checkbox.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './checkbox.styles.scss';
+
+const Checkbox = () => (
+    <span>
+        <input type="checkbox" />
+        <span></span>
+    </span>
+);
+
+export default Checkbox;
