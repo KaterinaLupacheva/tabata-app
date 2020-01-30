0906f5521bb722e145d83bd2ed6d7e4f1d820ea0
Katerina <katerinalupacheva@gmail.com>
2020-01-30 22:30:44 +0300

    Video context created

---
+++ front-end/src/contexts/video.context.js
@@ -0,0 +1,28 @@
+import React, { createContext } from 'react';
+
+export const VideoContext = createContext();
+
+export class VideoContextProvider extends React.Component {
+    state = {
+        isPaused: true
+    }
+
+    togglePlayVideo = () => {
+        this.setState(state => ({
+            isPaused: !state.isPaused
+          }));
+    };
+
+    render() {
+        return (
+            <VideoContext.Provider value = {{
+                state: this.state,
+                update: this.togglePlayVideo
+            }}>
+                {this.props.children}
+            </VideoContext.Provider>
+        )
+    }
+}
+
+export const VideoContextConsumer = VideoContext.Consumer;
