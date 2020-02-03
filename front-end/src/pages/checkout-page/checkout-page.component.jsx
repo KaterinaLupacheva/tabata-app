9d6e89fc21a002db65b81c49437204656d8f4563
Katerina <katerinalupacheva@gmail.com>
2020-02-03 17:39:43 +0300

    UseState and useEffect hooks added to Checkout page

--- front-end/src/pages/checkout-page/checkout-page.component.jsx
+++ front-end/src/pages/checkout-page/checkout-page.component.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { Elements, StripeProvider } from "react-stripe-elements";
 
 import './checkout-page.styles.scss';
 
-const CheckoutPage = () => (
-    <div>
-        CHECKOUT
+const CheckoutPage = () => {
+    const [apiKey, setApiKey] = useState(null);
+
+    useEffect(() => {
+        setApiKey(() => 'test');
+    });
+
+    return (
+    <div className='checkout'>
+        {apiKey && (
+          <StripeProvider apiKey={apiKey}>
+            <Elements>
+              {/* <CheckoutForm /> */}
+            </Elements>
+          </StripeProvider>
+        )}
     </div>
-);
+)};
 
 export default CheckoutPage;
