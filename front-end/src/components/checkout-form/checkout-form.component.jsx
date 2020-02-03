a1885ade02e50dff13cbf05d35f4aa4388d6cf4f
Katerina <katerinalupacheva@gmail.com>
2020-02-03 19:27:14 +0300

    RenderForm and renderSuccess methods added to checkout form

---
+++ front-end/src/components/checkout-form/checkout-form.component.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { CardElement, injectStripe } from "react-stripe-elements";
+
+import './checkout-form.styles.scss';
+
+
+const CheckoutForm = () => {
+    const [succeeded, setSucceeded] = useState(false);
+    const [disabled, setDisabled] = useState(false);
+    const [processing, setProcessing] = useState(false);
+    const [error, setError] = useState(null);
+    const [amount, setAmount] = useState(100);
+    const [currency, setCurrency] = useState('usd');
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('SUBMIT')
+    };
+
+    const renderSuccess = () => {
+        return (
+          <div className="success-message">
+            <h1>Your test payment succeeded</h1>
+            {/* <p>View PaymentIntent response:</p>
+            <pre className="sr-callout">
+              <code>{JSON.stringify(this.state.metadata, null, 2)}</code>
+            </pre> */}
+          </div>
+        );
+      }
+
+    const renderForm = () => {
+        const style = {
+            base: {
+              color: "#32325d",
+              fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+              fontSmoothing: "antialiased",
+              fontSize: "16px",
+              "::placeholder": {
+                color: "#aab7c4"
+              }
+            },
+            invalid: {
+              color: "#fa755a",
+              iconColor: "#fa755a"
+            }
+          };
+
+        return (
+            <form className='checkout-form' onSubmit={handleSubmit}>
+                <h1>
+                    {currency.toLocaleUpperCase()}{" "}
+                    {amount.toLocaleString(navigator.language, {
+                        minimumFractionDigits: 2
+                    })}{" "}
+                </h1>
+                <h4>Subscribe to Tabata</h4>
+
+                <CardElement className='card-element' style={style} />
+
+                {error && (
+                    <div className="field-error">{error}</div>
+                )}
+
+                {!succeeded && (
+                    <button className='checkout-pay-button' disabled={disabled}>
+                    {processing ? "Processing…" : "Pay"}
+                </button>
+                )}
+                
+            </form>
+        );
+
+    };
+
+    return (
+        <div>
+            {succeeded && renderSuccess()}
+            {!succeeded && renderForm()}
+        </div>
+)};
+
+export default injectStripe(CheckoutForm);
