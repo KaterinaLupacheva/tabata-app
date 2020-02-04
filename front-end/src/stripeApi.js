4037b0068999978ac34b059ad07752a36a47629b
Katerina <katerinalupacheva@gmail.com>
2020-02-04 15:09:26 +0300

    Stripe Api with getPublishableKey method added

---
+++ front-end/src/stripeApi.js
@@ -0,0 +1,34 @@
+import { path } from './pages/url';
+
+const getPublicStripeKey = options => {
+return window
+    .fetch(`${path}/public-key`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => {
+    if (res.status === 200) {
+        return res.json();
+    } else {
+        return null;
+    }
+    })
+    .then(data => {
+    if (!data || data.error) {
+        console.log("API error:", { data });
+        throw Error("API Error");
+    } else {
+        return data.publicKey;
+    }
+    });
+};
+
+const api = {
+    // createPaymentIntent,
+    getPublicStripeKey: getPublicStripeKey,
+    // getProductDetails: getProductDetails
+};
+
+export default api;
