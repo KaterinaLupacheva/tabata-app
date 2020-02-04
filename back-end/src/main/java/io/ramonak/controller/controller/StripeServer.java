f2186e9a901f69eea309cdb0be35af6bf2c2c8df
Katerina <katerinalupacheva@gmail.com>
2020-02-04 16:33:20 +0300

    ProductDetails and getProductDetailsApi() method added in Stripe Server

--- back-end/src/main/java/io/ramonak/controller/controller/StripeServer.java
+++ back-end/src/main/java/io/ramonak/controller/controller/StripeServer.java
@@ -1,6 +1,7 @@
 package io.ramonak.controller.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,39 @@ public class StripeServer {
 
     Dotenv dotenv = Dotenv.load();
 
+    static class ProductDetails {
+        @SerializedName("amount")
+        Long amount;
+
+        @SerializedName("currency")
+        String currency;
+
+        public String getCurrency() {
+            return currency;
+        }
+
+        public Long getAmount() {
+            return amount;
+        }
+    }
+
+    private static ProductDetails getProductDetails() {
+        ProductDetails details = new ProductDetails();
+        details.amount = (long) 1990;
+        details.currency = "USD";
+        return details;
+    }
+
     @GetMapping("/public-key")
     public String getPublicKey() {
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("publicKey", dotenv.get("STRIPE_PUBLISHABLE_KEY"));
         return gson.toJson(responseData);
+    }
+
+    @GetMapping("/product-details")
+    public String getProductDetailsApi() {
+        ProductDetails productDetails = getProductDetails();
+        return gson.toJson(productDetails);
     }
 }
