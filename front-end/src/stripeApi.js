321057dd0ef49f8dfebd6c50a9160e2b0b85e349
Katerina <katerinalupacheva@gmail.com>
2020-02-04 16:40:15 +0300

    GetProductDetails method added to StripeApi

--- front-end/src/stripeApi.js
+++ front-end/src/stripeApi.js
@@ -25,10 +25,35 @@ return window
     });
 };
 
+const getProductDetails = options => {
+    return window
+      .fetch(`${path}/product-details`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then(data => {
+        if (!data || data.error) {
+          console.log("API error:", { data });
+          throw Error("API Error");
+        } else {
+          return data;
+        }
+      });
+  };
+
 const api = {
     // createPaymentIntent,
     getPublicStripeKey: getPublicStripeKey,
-    // getProductDetails: getProductDetails
+    getProductDetails: getProductDetails
 };
 
 export default api;
