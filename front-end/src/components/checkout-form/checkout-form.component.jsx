4877d8a08ee97e338a10fc6e564ba738d3f81d01
Katerina <katerinalupacheva@gmail.com>
2020-02-05 21:43:03 +0300

    Create Payment Intent method added

--- front-end/src/components/checkout-form/checkout-form.component.jsx
+++ front-end/src/components/checkout-form/checkout-form.component.jsx
@@ -5,13 +5,15 @@ import api from '../../stripeApi';
 import './checkout-form.styles.scss';
 
 
-const CheckoutForm = () => {
+const CheckoutForm = ({stripe}) => {
     const [succeeded, setSucceeded] = useState(false);
     const [disabled, setDisabled] = useState(false);
     const [processing, setProcessing] = useState(false);
     const [error, setError] = useState(null);
     const [amount, setAmount] = useState(100);
     const [currency, setCurrency] = useState('usd');
+    const [clientSecret, setClientSecret] = useState(null);
+    const [metadata, setMetadata] = useState(null);
 
     useEffect(()=> {
         api.getProductDetails().then(productDetails => {
@@ -20,19 +22,50 @@ const CheckoutForm = () => {
           });
     });
 
-    const handleSubmit = (ev) => {
+    const handleSubmit = async (ev) => {
         ev.preventDefault();
-        console.log('SUBMIT')
-    };
+
+        // Step 1: Create PaymentIntent over Stripe API
+        api
+        .createPaymentIntent({
+        payment_method_types: ["card"]
+        })
+        .then(clSec => {
+            setClientSecret(clSec);
+            setDisabled(true);
+            setProcessing(true);
+
+            // Step 2: Use clientSecret from PaymentIntent to handle payment in stripe.handleCardPayment() call
+        stripe
+        .handleCardPayment(clientSecret)
+        .then(payload => {
+          if (payload.error) {
+              setError(`Payment failed: ${payload.error.message}`);
+              setDisabled(false);
+              setProcessing(false);
+            console.log("[error]", payload.error);
+          } else {
+              setProcessing(false);
+              setSucceeded(true);
+              setError('');
+              setMetadata(payload.paymentIntent);
+            console.log("[PaymentIntent]", payload.paymentIntent);
+          }
+        });
+    })
+    .catch(err => {
+        setError(err.message);
+    });
+}
 
     const renderSuccess = () => {
         return (
           <div className="success-message">
             <h1>Your test payment succeeded</h1>
-            {/* <p>View PaymentIntent response:</p>
+            <p>View PaymentIntent response:</p>
             <pre className="sr-callout">
-              <code>{JSON.stringify(this.state.metadata, null, 2)}</code>
-            </pre> */}
+              <code>{JSON.stringify(metadata, null, 2)}</code>
+            </pre>
           </div>
         );
       }
--- front-end/src/stripeApi.js
+++ front-end/src/stripeApi.js
@@ -50,8 +50,36 @@ const getProductDetails = options => {
       });
   };
 
+  const createPaymentIntent = options => {
+      console.log('OPTIONS ' + JSON.stringify(options))
+    return window
+      .fetch(`${path}/create-payment-intent`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(options)
+      })
+      .then(res => {
+          console.log('RES ' + JSON.stringify(res))
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then(data => {
+        if (!data || data.error) {
+          console.log("API error:", { data });
+          throw new Error("PaymentIntent API Error");
+        } else {
+          return data.client_secret;
+        }
+      });
+  };
+
 const api = {
-    // createPaymentIntent,
+    createPaymentIntent,
     getPublicStripeKey: getPublicStripeKey,
     getProductDetails: getProductDetails
 };
