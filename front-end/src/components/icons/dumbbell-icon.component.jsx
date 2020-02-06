209e635bc000b99e9528cf607da24134cfc178f0
Katerina <katerinalupacheva@gmail.com>
2020-02-06 15:15:29 +0300

    Dummbell-icon component: onClick toggle filled and empty icon

--- front-end/src/components/icons/dumbbell-icon.component.jsx
+++ front-end/src/components/icons/dumbbell-icon.component.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { IconContext } from "react-icons";
-import { FaDumbbell } from "react-icons/fa";
+import React, { useState } from 'react';
+import image1 from '../../assets/dumbbell-filled-ramonak.png';
+import { ReactComponent as Dumbbell } from '../../assets/dumbbell.svg';
 import './dumbbell-icon.styles.scss';
 
-const DumbbellIcon = () => (
-    <IconContext.Provider value={{ size: '20px', className: "dumbbell-icon" }}>
-        <FaDumbbell />
-    </IconContext.Provider>
-);
+const DumbbellIcon = () => {
+    const[clicked, toggleClicked] = useState(false);
+    return(
+        <>
+        {clicked ? 
+            <img src={image1} alt='dumbbell' className='dumbbell-icon' onClick={() => toggleClicked(!clicked)} /> :
+            <Dumbbell className='dumbbell-empty' onClick={() => toggleClicked(!clicked)}/>
+        }
+        </>    
+)};
 
 export default DumbbellIcon;
