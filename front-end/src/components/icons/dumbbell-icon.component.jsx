cd6369c8c6990cbe8b27d785b63f1206ab77d267
Katerina <katerinalupacheva@gmail.com>
2020-02-06 16:07:16 +0300

    WithWeights context consumer added to dumbbell-icon component

--- front-end/src/components/icons/dumbbell-icon.component.jsx
+++ front-end/src/components/icons/dumbbell-icon.component.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import image1 from '../../assets/dumbbell-filled-ramonak.png';
 import { ReactComponent as Dumbbell } from '../../assets/dumbbell.svg';
+import WithWeightsCheckboxContext from '../../contexts/with-weights-checkbox.context';
 import './dumbbell-icon.styles.scss';
 
-const DumbbellIcon = () => {
-    const[clicked, toggleClicked] = useState(false);
-    return(
-        <>
-        {clicked ? 
-            <img src={image1} alt='dumbbell' className='dumbbell-icon' onClick={() => toggleClicked(!clicked)} /> :
-            <Dumbbell className='dumbbell-empty' onClick={() => toggleClicked(!clicked)}/>
-        }
-        </>    
-)};
+const DumbbellIcon = () => (
+    <WithWeightsCheckboxContext.Consumer>
+    {({ checked, toggleChecked }) => checked ? 
+        <img src={image1} alt='dumbbell' className='dumbbell-icon' onClick={toggleChecked} /> :
+        <Dumbbell className='dumbbell-empty' onClick={toggleChecked}/>
+    }
+    </WithWeightsCheckboxContext.Consumer>    
+);
 
 export default DumbbellIcon;
