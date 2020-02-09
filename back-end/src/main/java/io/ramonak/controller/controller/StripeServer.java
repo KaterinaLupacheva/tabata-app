3fcd2dd610dc3b121fc2d6d93aa03b1b5a89aa2c
Katerina <katerinalupacheva@gmail.com>
2020-02-09 21:38:37 +0300

    Create payment intent method added to Stripe server

--- back-end/src/main/java/io/ramonak/controller/controller/StripeServer.java
+++ back-end/src/main/java/io/ramonak/controller/controller/StripeServer.java
@@ -2,12 +2,17 @@ package io.ramonak.controller.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -33,6 +38,15 @@ public class StripeServer {
         }
     }
 
+    static class CreatePaymentBody {
+        @SerializedName("payment_method_types")
+        String payment_method_types;
+
+        public String getPaymentMethod() {
+            return payment_method_types;
+        }
+    }
+
     private static ProductDetails getProductDetails() {
         ProductDetails details = new ProductDetails();
         details.amount = (long) 199;
@@ -51,5 +65,36 @@ public class StripeServer {
     public String getProductDetailsApi() {
         ProductDetails productDetails = getProductDetails();
         return gson.toJson(productDetails);
+    }
+
+    @PostMapping("/create-payment-intent")
+    public String createPaymentIntent(@RequestBody Map<String, Object> request) {
+        Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
+        ProductDetails productDetails = getProductDetails();
+        CreatePaymentBody postBody = new CreatePaymentBody();
+        postBody.payment_method_types = request.get("payment_method_types").toString();
+//        CreatePaymentBody postBody = gson.fromJson(request.getBody(), CreatePaymentBody.class);
+
+        List<Object> paymentMethodTypes =
+                new ArrayList<>();
+        paymentMethodTypes.add(postBody.getPaymentMethod());
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", productDetails.getAmount());
+        params.put("currency", productDetails.getCurrency());
+        params.put(
+                "payment_method_types",
+                paymentMethodTypes
+        );
+
+//        PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+//                .setCurrency(productDetails.getCurrency()).setAmount(productDetails.getAmount())
+//                .setPaymentMethod(postBody.getPaymentMethod()).build();
+        PaymentIntent intent = null;
+        try {
+            intent = PaymentIntent.create(params);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
+        return gson.toJson(intent);
     }
 }
